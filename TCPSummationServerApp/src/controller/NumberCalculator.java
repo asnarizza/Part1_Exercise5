f26b5c4ba7c506e12988797de80643741066b321
Rose Asnarizza <asnarizza@gmail.com>
2023-06-20 18:00:03 +0800

    Part1 Exercise5

---
+++ TCPSummationServerApp/src/controller/NumberCalculator.java
@@ -0,0 +1,32 @@
+package controller;
+
+
+/**
+ * This is a controller class that manage the arithmetic operations.
+ * 
+ * @author Rose Asnarizza Nahar
+ *
+ */
+
+public class NumberCalculator {
+	
+	
+	/**
+	 * A method that will get the summation from two numbers
+	 * @param number1
+	 * @param number2
+	 * @return
+	 */
+	public int getSum(int number1, int number2, int number3) {
+		
+		return number1 + number2 + number3;
+		
+	}
+	
+	public int getProduct(int number1, int number2, int number3) {
+		
+		return number1 * number2 * number3;
+		
+	}
+
+}
